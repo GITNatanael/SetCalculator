87f927d13e2877da5f1acff1bd5c2c6dc6d3158a
Natanael LR <nat.contactme@gmail.com>
2023-08-19 18:45:45 -0500

    index.html routes changed

--- Scripts/Menu.js
+++ Scripts/Menu.js
@@ -3,10 +3,10 @@ const logoBlack = document.querySelector('.Logo.Black');
 const logoWhite = document.querySelector('.Logo.White');
 
 logoBlack.addEventListener('click', function () {
-    window.location.href = 'home.html';
+    window.location.href = 'index.html';
 });
 
 logoWhite.addEventListener('click', function () {
-    window.location.href = 'home.html';
+    window.location.href = 'index.html';
 });
 
